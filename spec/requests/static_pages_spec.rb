51eb2b607656f346ebfee5c7fb4278e4d9249c1f
shota yamamoto <panpankun8n@gmail.com>
2014-01-10 17:00:25 +0900

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
-
 describe "StaticPages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
+  shared_examples_for "all static pages" do
 
-    it { should have_content  ('Sample App') }
-    it { should have_title  (full_title('')) }
-    it { should_not have_title   ('| Home') }
+    it { should have_content  (heading) }
+    it { should have_title  (full_title(page_title)) }
   end
 
+  describe "Home page" do
+    before {visit root_path}
+    let(:heading)   {'Sample App'}
+    let(:page_title) {''}
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('|Home')}
+  end 
 
   describe "Help page" do 
     before { visit help_path }
@@ -22,7 +27,7 @@ describe "StaticPages" do
 
 
   describe "About page" do
-     before { visit about_path }
+    before { visit about_path }
 
     it { should have_content ('About') }
     it { should have_title (full_title('About Us')) }
@@ -34,5 +39,5 @@ describe "StaticPages" do
 
     it { should have_content ('Contact') }
     it { should have_title (full_title('Contact')) }
-  end   
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,9 +5,9 @@ describe "User pages" do
   subject { page }
 
   describe "signup page" do
-  	before { visit signup_path }
+  	 before { visit signup_path }
 
-  	it { should have_content('Sign up') }
-  	it { should have_title(full_title('Sign up')) }
-  end	
+  	 it { should have_content('Sign up') }
+  	 it { should have_title(full_title('Sign up')) }
+    end	
 end
